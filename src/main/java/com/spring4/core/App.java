427282c520f1846e2be8cb0cb3452250cd7456a6
Ronak Soni <Ronak.Soni@morningstar.com>
2017-02-23 00:00:49 +0530

    Initial Configuration

---
+++ src/main/java/com/spring4/core/App.java
@@ -0,0 +1,12 @@
+package com.spring4.core;
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("")
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
